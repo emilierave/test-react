3532dbc9844714dae4226f1130a471b9768b93c6
emilierave <marjabronikowska@gmail.com>
2022-05-13 18:33:28 +0200

    feat(app): add link to gitHub

--- src/App.js
+++ src/App.js
@@ -7,22 +7,31 @@ function App() {
     alert('Hello react')
   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button onClick={handleOpenAlert}>Open alert</button>
+          <a
+              className="App-link"
+              href="https://github.com/emilierave"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            test-react-project
+          </a>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
